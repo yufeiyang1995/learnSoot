f008809a1203cc7e2318cf95308df9f6cae49262
yufeiyang <1350763025@qq.com>
2017-10-10 23:27:35 +0800

    try make jar

--- src/learnSoot/Main.java
+++ src/learnSoot/Main.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import org.xmlpull.v1.XmlPullParserException;
-
 import soot.PackManager;
 import soot.Scene;
 import soot.Transform;
